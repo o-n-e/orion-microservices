80ee84f51ee5a36a6cfc4c2b108ac7443b9631fa
stewartbarron <stewart.barron@mac.com>
2023-12-27 17:59:52 +0000

    inter-service communication

--- inventory-service/src/main/java/org/arunachala/inventoryservice/service/InventoryService.java
+++ inventory-service/src/main/java/org/arunachala/inventoryservice/service/InventoryService.java
@@ -1,9 +1,12 @@
 package org.arunachala.inventoryservice.service;
 
+import org.arunachala.inventoryservice.dto.InventoryResponse;
 import org.springframework.transaction.annotation.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.arunachala.inventoryservice.repository.InventoryRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -11,7 +14,14 @@ public class InventoryService {
     private final InventoryRepository inventoryRepository;
 
     @Transactional(readOnly = true)
-    public boolean isInStock(String skuCode) {
-        return inventoryRepository.findBySkuCode(skuCode).isPresent();
+    public List<InventoryResponse> isInStock(List<String> skuCode) {
+        return inventoryRepository.findBySkuCodeIn(skuCode)
+                .stream()
+                .map(inventory ->
+                    InventoryResponse.builder()
+                            .skuCode(inventory.getSkuCode())
+                            .isInStock(inventory.getQuantity() > 0)
+                            .build()
+                ).toList();
     }
 }
--- order-service/src/main/java/org/arunachala/orderservice/service/OrderService.java
+++ order-service/src/main/java/org/arunachala/orderservice/service/OrderService.java
@@ -1,21 +1,27 @@
 package org.arunachala.orderservice.service;
 
 import lombok.RequiredArgsConstructor;
+import org.arunachala.orderservice.dto.InventoryResponse;
 import org.arunachala.orderservice.dto.OrderLineItemsDto;
 import org.arunachala.orderservice.dto.OrderRequest;
 import org.arunachala.orderservice.model.Order;
 import org.arunachala.orderservice.model.OrderLineItems;
 import org.arunachala.orderservice.repository.OrderRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.reactive.function.client.WebClient;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
+@Transactional
 public class OrderService {
 
     private final OrderRepository orderRepository;
+    private final WebClient.Builder webClientBuilder;
 
     public void createOrder(OrderRequest orderRequest) {
         Order order = new Order();
@@ -28,7 +34,26 @@ public class OrderService {
 
         order.setOrderLineItemsList(orderLineItems);
 
-        orderRepository.save(order);
+        List<String> skuCodes = order.getOrderLineItemsList()
+                .stream()
+                .map(OrderLineItems::getSkuCode)
+                .toList();
+
+        // Call inventory service and create order if in stock
+        InventoryResponse[] inventoryResponseArray = webClientBuilder.build().get()
+                .uri("http://localhost:8082/api/inventory",
+                        uriBuilder -> uriBuilder.queryParam("skuCode", skuCodes).build())
+                .retrieve()
+                .bodyToMono(InventoryResponse[].class)
+                .block();
+
+        boolean allProductsInStock = Arrays.stream(inventoryResponseArray).allMatch(InventoryResponse::isInStock);
+
+        if (allProductsInStock) {
+            orderRepository.save(order);
+        } else {
+            throw new IllegalArgumentException("Product not in stock");
+        }
     }
 
     private OrderLineItems mapToDto(OrderLineItemsDto orderLineItemsDto) {
